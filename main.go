25a30e4d47b684c9ebf5c0fe1dea0f982c66a51e
emran <emranhi001@gmail.com>
2024-08-25 11:45:32 +0300

    made changes to go configurations

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"AAiT-backend-group-6/bootstrap"
-	"AAiT-backend-group-6/delivery/route"
+	"blog-api_with-clean-architecture/bootstrap"
+	"blog-api_with-clean-architecture/delivery/route"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -17,14 +17,14 @@ func main() {
 	// if app.Redis == nil {
 	// 	panic("Redis is not connected")
 	// }
-	redisClient := app.Redis
-	defer app.Close()
+	// redisClient := app.Redis
+	// defer app.Close()
 
 	timeout := time.Duration(env.ContextTimeout) * time.Second
 
 	gin := gin.Default()
 
-	route.Setup(env, timeout, db, gin, redisClient)
+	route.Setup(env, timeout, db, gin)
 	gin.Run(env.ServerAddress)
 }
 
